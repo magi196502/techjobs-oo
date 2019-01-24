84258006c83ebe8937d30028191ff2e65d647470
Macy Graham-Kimbrough <macyg196502@yahoo.com>
2019-01-24 10:57:28 -0600

    Complete get job detail by job id.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -24,6 +26,11 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job job = jobData.findById(id);
+
+//      model.addAttribute("title", "Jobs with " + column.getName() + ": " + name);
+        model.addAttribute("job", job);
+
 
         return "job-detail";
     }
